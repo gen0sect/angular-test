377734e1a5121a0b3f26d6ac444c77bcf9ef77ed
gen0sect <jjnajan1@gmail.com>
2025-02-04 10:30:19 +0800

    tested deferrable views

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,36 @@
 import {Component} from '@angular/core';
-import {UserComponent} from './user.component';
-import { ChildComponent } from './child.component';
+import {CommentsComponent} from './comments.component';
 
 @Component({
   selector: 'app-root',
   template: `
-    <section (mouseover)="onMouseOver()">
-      There's a secret message for you, hover to reveal 👀
-      {{ message }}
-    </section>
-    <section>
-      <button (click)="greet()">Greet</button>
-    </section>
-    <app-user occupation="Angular Developer" name="Joshua"></app-user>
-    <app-child (addItemEvent)="addItem($event)" />
+    <div>
+      <h1>How I feel about Angular</h1>
+      <article>
+      <p>Angular is my favorite framework, and this is why. Angular has the coolest deferrable view feature that makes defer loading content the easiest and most ergonomic it could possibly be. The Angular community is also filled with amazing contributors and experts that create excellent content. The community is welcoming and friendly, and it really is the best community out there.</p>
+        <p>I can't express enough how much I enjoy working with Angular. It offers the best developer experience I've ever had. I love that the Angular team puts their developers first and takes care to make us very happy. They genuinely want Angular to be the best framework it can be, and they're doing such an amazing job at it, too. This statement comes from my heart and is not at all copied and pasted. In fact, I think I'll say these exact same things again a few times.</p>
+        <p>Angular is my favorite framework, and this is why. Angular has the coolest deferrable view feature that makes defer loading content the easiest and most ergonomic it could possibly be. The Angular community is also filled with amazing contributors and experts that create excellent content. The community is welcoming and friendly, and it really is the best community out there.</p>
+        <p>I can't express enough how much I enjoy working with Angular. It offers the best developer experience I've ever had. I love that the Angular team puts their developers first and takes care to make us very happy. They genuinely want Angular to be the best framework it can be, and they're doing such an amazing job at it, too. This statement comes from my heart and is not at all copied and pasted. In fact, I think I'll say these exact same things again a few times.</p>
+        <p>Angular is my favorite framework, and this is why. Angular has the coolest deferrable view feature that makes defer loading content the easiest and most ergonomic it could possibly be. The Angular community is also filled with amazing contributors and experts that create excellent content. The community is welcoming and friendly, and it really is the best community out there.</p>
+        <p>I can't express enough how much I enjoy working with Angular. It offers the best developer experience I've ever had. I love that the Angular team puts their developers first and takes care to make us very happy. They genuinely want Angular to be the best framework it can be, and they're doing such an amazing job at it, too. This statement comes from my heart and is not at all copied and pasted.</p>
+        <p>Angular is my favorite framework, and this is why. Angular has the coolest deferrable view feature that makes defer loading content the easiest and most ergonomic it could possibly be. The Angular community is also filled with amazing contributors and experts that create excellent content. The community is welcoming and friendly, and it really is the best community out there.</p>
+        <p>I can't express enough how much I enjoy working with Angular. It offers the best developer experience I've ever had. I love that the Angular team puts their developers first and takes care to make us very happy. They genuinely want Angular to be the best framework it can be, and they're doing such an amazing job at it, too. This statement comes from my heart and is not at all copied and pasted. In fact, I think I'll say these exact same things again a few times.</p>
+        <p>Angular is my favorite framework, and this is why. Angular has the coolest deferrable view feature that makes defer loading content the easiest and most ergonomic it could possibly be. The Angular community is also filled with amazing contributors and experts that create excellent content. The community is welcoming and friendly, and it really is the best community out there.</p>
+        <p>I can't express enough how much I enjoy working with Angular. It offers the best developer experience I've ever had. I love that the Angular team puts their developers first and takes care to make us very happy. They genuinely want Angular to be the best framework it can be, and they're doing such an amazing job at it, too. This statement comes from my heart and is not at all copied and pasted. In fact, I think I'll say these exact same things again a few times.</p>
+        <p>Angular is my favorite framework, and this is why. Angular has the coolest deferrable view feature that makes defer loading content the easiest and most ergonomic it could possibly be. The Angular community is also filled with amazing contributors and experts that create excellent content. The community is welcoming and friendly, and it really is the best community out there.</p>
+        <p>I can't express enough how much I enjoy working with Angular. It offers the best developer experience I've ever had. I love that the Angular team puts their developers first and takes care to make us very happy. They genuinely want Angular to be the best framework it can be, and they're doing such an amazing job at it, too. This statement comes from my heart and is not at all copied and pasted.</p>
+      </article>  
+      @defer (on viewport) {
+        <comments />
+      }
+      @placeholder {
+        <p> Future comments will be displayed here </p>
+      }
+      @loading (minimum 2s) {
+        <p> Loading comments... </p>
+      }
+    </div>
   `,
-  imports: [UserComponent, ChildComponent],
+  imports: [CommentsComponent],
 })
-export class AppComponent {
-  message = '';
-
-  onMouseOver() {
-    this.message = '🎉 You are awesome'
-  }
-
-greet() {
-  console.log('Hello, there 👋');
-}
-
-addItem(item: any) {
-  console.log('Item added:', item);
-}
-}
+export class AppComponent {}
