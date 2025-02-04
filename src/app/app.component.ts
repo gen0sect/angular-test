ca89f3f81e58ae18221fe6bc8e03d8a76ee34043
gen0sect <jjnajan1@gmail.com>
2025-02-04 09:33:49 +0800

    tested if-else statements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import {Component} from '@angular/core';
-import {UserComponent} from './user.component';
 
 @Component({
   selector: 
   'app-root',
-  template: 
-  `<section><app-user/></section>`,
-  imports:[UserComponent]
-})
+  template: `
+  @if (isLoggedin) {
+   <p> You are currently Logged in! </p>
+  }
+  @else {
+   <p> You are currently Logged out! </p>
+  }
+  `
+  })
 
 export class AppComponent {
+  isLoggedin=false;
 }
